94a58ffee2cba455cc229a938ccd74458894ef3e
Antua <suleykin2005@mail.ru>
2026-02-18 20:23:58 +0300

    add json file and data class for prep

---
+++ app/src/main/java/com/example/a43/Repository.kt
@@ -0,0 +1,15 @@
+package com.example.a43
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Repository(
+    val id: Int,
+    @SerialName(value = "full_name")
+    val name: String,
+    val description: String,
+    @SerialName(value = "stargazers_count")
+    val stargazersCount: Int,
+    val language: String?
+)
